9772b0fb0cc691796b9e7e200e75b0f30cccb085
jacopolegovini <legovini96@gmail.com>
2024-10-02 15:14:29 +0200

    index created

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
-    echo 'test';
+    echo 'hello';
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bad Words</title>
+</head>
+<body>
+    <form action="bad_words.php" method="POST">
+        <input type="text" name="word" placeholder="Inserisci una parola">
+        <input type="text" name="bad_word" placeholder="Inserisci la parola da censurare">
+        <button>Invia</button>
+    </form>
+</body>
+</html>
